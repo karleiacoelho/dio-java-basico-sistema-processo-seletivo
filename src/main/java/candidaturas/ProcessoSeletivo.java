b560150f28abdd96b8f5151313837e3c7810bbf8
Karleia Coleho <karleia.c.o.s@gamil.com>
2024-05-29 15:08:27 -0300

    Implemmntado método 'analisarCandidato' recebendo o parametro 'salarioPretendido'

--- src/main/java/candidaturas/ProcessoSeletivo.java
+++ src/main/java/candidaturas/ProcessoSeletivo.java
@@ -1,9 +1,19 @@
 package candidaturas;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
+        System.out.println("*** Processo Seletivo ***");
 
-        System.out.printf("Hello and welcome!");
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+        static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+            if(salarioBase > salarioPretendido) {
+            System.out.println("Ligar para candidato.");
+            } else if(salarioPretendido == salarioBase) {
+            System.out.println("Ligar para candidato com contra proposta.");
+            } else System.out.println("Aguardando demais resultados.");
 
         }
-    
 }
