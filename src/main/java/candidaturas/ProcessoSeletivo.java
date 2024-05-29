ad878ddc2f1cdb3800b6141482e50245c07f41f1
Karleia Coleho <karleia.c.o.s@gamil.com>
2024-05-29 16:20:33 -0300

    Adicionado método 'selecaoCandidatos' com uso do while para percorrer a lista de candidatos analisando um a um, conforme as regras de negócio

--- src/main/java/candidaturas/ProcessoSeletivo.java
+++ src/main/java/candidaturas/ProcessoSeletivo.java
@@ -1,13 +1,40 @@
 package candidaturas;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.util.concurrent.ThreadLocalRandom.current;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("*** Processo Seletivo ***");
-
+        /*
         analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);
+        */
+        selecaoCandidatos();
     }
-        static void analisarCandidato(double salarioPretendido) {
+    static void selecaoCandidatos() {
+        String[] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosSelecionados < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candodato " + candidato + " pretende o salario de R$ " + salarioPretendido);
+
+            if(salarioBase <= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800.0 , 2200.0);
+    }
+    static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
             if(salarioBase > salarioPretendido) {
             System.out.println("Ligar para candidato.");
